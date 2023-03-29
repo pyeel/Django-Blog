2ee5616c7fdc4f455e5f666c92400ce165c5989b
eunsung <a01066697741@gmail.com>
2023-03-29 11:38:19 +0900

    CBV로 블로그 포스트 목록페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView): # ListView 클래스를 상속해서 PostList 클래스 생성
+    model = Post
+    template_name = 'blog/post_list.html'
+    ordering = '-pk' # pk값의 역순으로 정렬
+
+#def index(request):
+#   posts = Post.objects.all().order_by('-pk')
     # order_by('pk') -> pk값의 역순으로 정렬
     # views.py에서 데이터베이스에 쿼리를 보내 원하는 레코드를 가져올 수 있음
     # 쿼리 : 데이터베이스의 데이터를 가져오거나 수정, 삭제하는 등의 행위를 하기 위한 요청
     
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+#    return render(
+#        request,
+#        'blog/index.html',
+#        {
+#            'posts': posts,
+#        }
+ #   )
     # 장고가 기본으로 제공하는 render() 함수를 사용
     # -> 템플릿 폴더에서 blog 폴더의 index.html 파일을 찾아 방문자에게 전송
 
