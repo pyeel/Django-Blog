43d8d11b2948193c2b5e355f0b0b7e3e0663eab8
eunsung <a01066697741@gmail.com>
2023-04-12 11:21:33 +0900

    포스트 목록 페이지와 포스트 상세 페이지 디자인 통일하기

--- blog/tests.py
+++ blog/tests.py
@@ -73,7 +73,7 @@ class TestView(TestCase):
         self.assertIn(post_001.title, soup.title.text
                       )
         
-        main_area = soup.find('div', id='main=area')
+        main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
         self.assertIn(post_001.title, post_area.text)
         
